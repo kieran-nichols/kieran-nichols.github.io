88f9ae42f40906839d324f4f17f08358087660b0
KIERAN\the1k <the1kieran@gmail.com>
2018-08-19 13:35:36 -0500

    hid pictures and update email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'the1kieran@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'knichols4@wisc.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: website\n"; // This is the email address the generated message will be from. We recommend using something like the1kieran@gmail.com.
+$headers = "From: website\n"; // This is the email address the generated message will be from. We recommend using something like knichols4@wisc.edu.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
