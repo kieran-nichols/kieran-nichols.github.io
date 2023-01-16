43e17b6b9d913fcb53aab6a2c3204021bc0b3ee7
kieran-nichols <knichols4@wisc.edu>
2023-01-16 11:15:48 -0600

    added gitignore

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://kieran-nichols.github.io'
 RELATIVE_URLS = False
@@ -19,6 +18,10 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+DOMAIN = SITEURL
+FEED_DOMAIN = SITEURL
+HTTPS = True
 
 # Following items are often useful when publishing
 
