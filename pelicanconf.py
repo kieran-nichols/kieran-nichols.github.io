ed54db9fdb4e0fa7f7d3ca04f098e6a73805ef0d
kieran-nichols <knichols4@wisc.edu>
2023-01-16 10:39:38 -0600

    added portfolio, resume, and included use of a bulrush theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,15 +19,32 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = 'themes/Bulrush'
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['assets']
+
+import bulrush
+
+THEME = bulrush.PATH
+JINJA_ENVIRONMENT = bulrush.ENVIRONMENT
+JINJA_FILTERS = bulrush.FILTERS
+
+
+SOCIAL = (
+    ('Email', 'knichols4@wisc.edu'),
+    ("Github", "https://github.com/kieran-nichols/"),
+    ("LinkedIn", "https://www.linkedin.com/in/kieran-nichols-24134479/")
+)
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
