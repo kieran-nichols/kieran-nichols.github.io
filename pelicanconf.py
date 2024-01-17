bf58fd377745ff581c3258ebb1f81ba9ff015d33
kieran-nichols <knichols4@wisc.edu>
2024-01-17 07:49:39 -0600

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'kieran-nichols'
 SITENAME = 'Kieran Nichols'
 # SITEURL = 'https://www.kieran-nichols.com'
-SITEURL = 'www.kieran-nichols.github.io'
+SITEURL = 'https://www.kieran-nichols.github.io'
 
 PATH = 'content'
 
