baa349eb159aef4d7a168163e201e7e4110240ed
kieran-nichols <knichols4@wisc.edu>
2024-01-21 08:35:53 -0600

    update

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-#SITEURL = 'https://www.kieran-nichols.com'
+# SITEURL = 'https://www.kieran-nichols.com'
 SITEURL = 'https://www.kieran-nichols.github.io'
 RELATIVE_URLS = True
 
